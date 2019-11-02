6bb3df2df3c6651688ecad0164aa7d68b17636e6
tommy <tommywhy1989@gmail.com>
2019-11-03 00:28:01 +1300

    insertion sort

--- sort/insertionSort/main.cpp
+++ sort/insertionSort/main.cpp
@@ -7,16 +7,18 @@
 
 #include "../SortTestHelper.h"
 
+/*
+ * 对于 近似有序数组，插排效率最高。可以高于 nlogn。 On
+ */
 template <class T>
 void insertionSort(T arr[], int n){
     for (int i = 1; i < n; i++) {
-        for (int j = i; j > 0; j--) {
-            if(arr[j-1] > arr[j]){
-                swap(arr[j-1],arr[j]);
-            }else{
-                break;
-            }
+        T element = arr[i]; //
+        int j; // 疑似目标位置
+        for ( j = i; j > 0 && arr[j-1] > element; j--) {
+                arr[j] =arr[j-1]; // 后移
         }
+        arr[j] =element; // 交换
     }
 }
 
